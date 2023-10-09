4d1dbdbcea2d7e951f251905e3b55467e882ece5
Raisul Islam <robin35.291@gmail.com>
2023-10-10 01:43:40 +0600

    2nd commit

---
+++ Java.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Java {
+    public static void main(String[] args) {
+         String paragraph = """
+                      -
+                     | |
+                     | |  __ ___    ____ _
+                 -   | | / _' \\ \\  / / _' |
+                | |__| |  (_| |\\ \\/ / (_| |
+                \\_____/ \\__,_ | \\__/ \\__,_|
+
+                      """;
+        System.out.println(paragraph);
+
+    }
+}
